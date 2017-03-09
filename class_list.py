0be1eac39a42b15d27b01908bee16adcc8e9082e
k33theod <k33theod@gmail.com>
2017-03-09 15:48:28 +0100

    Update class_list.py

--- class_list.py
+++ class_list.py
@@ -1,25 +1,25 @@
-class multi_list(list):
-	def get_shape(self):
+class ML(list):
+	def shape(self):
 		bathos=0
 		for i in str(self):
 			if i!='[':
 				break
 			bathos+=1
-		shape=[]
+		_shape=[]
 		while bathos>0:
-			shape.append(len(self))
+			_shape.append(len(self))
 			self=self[0]
 			bathos-=1
-		return shape
-	def flatten_list(self):
+		return _shape
+	def flatten(self):
 			c=''.join([i for i in str(self)if i not in'[] ']).strip(',')
-			return multi_list(list(map(int,c.split(','))))
-	def grammes_stiles(self):
+			return ML(list(map(int,c.split(','))))
+	def rows_to_columns(self):
 		b=list(zip(*self))
-		return multi_list([list(i) for i in b])
+		return ML([list(i) for i in b])
 	from functools import reduce
-	def get_items(self):
-		return reduce(lambda x,y :x*y, get_shape(self)) 
+	def items(self):
+		return reduce(lambda x,y :x*y, ML.shape(self)) 
 		
 from pprint import pprint as pp
 from copy import deepcopy
